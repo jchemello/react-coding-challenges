950a4b068493a9096250e44cf74bda349b4bf04f
Juan E. Chemello <jchemello@summasolutions.net>
2021-03-08 17:37:42 -0300

    Completed challenge with comments on components/App.js

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
-import '../styles/_app.scss';
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import '../styles/_app.scss'
 
 function App() {
+  //?  Realicé la solución más fácil, la cual podría funcionar para otras rutas enviando por props theme y/o setTheme, sin embargo para una aplicación de mayor complejidad sería apropiado usar un Provider, Context API de React u otro state manager como Redux.
+  //? Mi forma de agregar y modificar una clase al html tag fue con vanilla js, no pude encontrar otra forma desde react de hacerlo.
+
+  const [theme, setTheme] = useState(null)
+
+  const toggleTheme = () => {
+    document.documentElement.classList.toggle('dark-mode')
+    if (document.documentElement.classList.contains('dark-mode')) {
+      setTheme('dark-mode')
+    } else {
+      setTheme(null)
+    }
+  }
+
   return (
     <div className="app">
       <div className="level">
@@ -13,17 +27,33 @@ function App() {
 
         {/* --The button that should toggle dark mode-- */}
         <button className="app__dark-mode-btn icon level-right">
-          <FontAwesomeIcon icon={faMoon} />
+          <FontAwesomeIcon
+            icon={theme ? faSun : faMoon}
+            color={theme && '#FFA500'}
+            onClick={toggleTheme}
+          />
         </button>
-
       </div>
 
       <div className="columns">
         <div className="column">
-          <p>Lollipop powder powder. Cotton candy caramels chupa chups halvah muffin caramels apple pie topping cake. Topping chocolate bar pastry chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar plum. Chocolate bar lollipop candy canes. Biscuit croissant apple pie pudding caramels wafer tart tootsie roll macaroon. Croissant tiramisu chocolate bar carrot cake lemon drops halvah.</p>
+          <p>
+            Lollipop powder powder. Cotton candy caramels chupa chups halvah
+            muffin caramels apple pie topping cake. Topping chocolate bar pastry
+            chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar
+            plum. Chocolate bar lollipop candy canes. Biscuit croissant apple
+            pie pudding caramels wafer tart tootsie roll macaroon. Croissant
+            tiramisu chocolate bar carrot cake lemon drops halvah.
+          </p>
         </div>
         <div className="column">
-          <p>Marshmallow tiramisu liquorice bear claw chocolate bar bear claw tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart. Pudding sugar plum chocolate cake cake biscuit pastry pastry chocolate bar tart. Lemon drops dessert gummies icing.</p>
+          <p>
+            Marshmallow tiramisu liquorice bear claw chocolate bar bear claw
+            tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops
+            marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart.
+            Pudding sugar plum chocolate cake cake biscuit pastry pastry
+            chocolate bar tart. Lemon drops dessert gummies icing.
+          </p>
         </div>
       </div>
 
@@ -41,12 +71,16 @@ function App() {
 
       <section className="section">
         <div className="buttons level-right">
-          <a className="button is-primary">Save</a>
-          <a className="button is-link">Submit</a>
+          <a href="#!" className="button is-primary">
+            Save
+          </a>
+          <a href="#!" className="button is-link">
+            Submit
+          </a>
         </div>
       </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
