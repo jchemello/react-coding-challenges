a574e860b4e492de533d1f5a5d523f8ab6236378
Juan E. Chemello <jchemello@summasolutions.net>
2021-03-08 17:38:21 -0300

    Completed 2nd challenge with comments on LaunchPad.js y Rocket.js

--- rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
+++ rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
-import { ClassRocket, FunctionalRocket } from './Rocket';
-import '../styles/_launchpad.scss';
+import React, { useState } from 'react'
+import { ClassRocket, FunctionalRocket } from './Rocket'
+import '../styles/_launchpad.scss'
 
 export default function LaunchPad() {
-  const [rerenderCount, triggerRerender] = useState(0);
+  const [rerenderCount, triggerRerender] = useState(0)
 
-  setTimeout(() => { triggerRerender(rerenderCount + 1); }, 500);
+  setTimeout(() => {
+    // triggerRerender(rerenderCount + 1)
+  }, 500)
 
   return (
     <div className="launchpad">
-      <ClassRocket />
+      <FunctionalRocket />
     </div>
-  );
+  )
 }
+
+//? El componente LaunchPad se renderiza infinitamente porque se modifica su estado cada 500ms, eliminando el setter triggerRerender o setteando el mismo valor ya existente se puede impedir el re-render. Esto pasa porque al actualizarse un estado de un componente React por defecto renderiza nuevamente el mismo y sus componentes hijos.
--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -1,24 +1,26 @@
-import React, { useState, Component } from 'react';
-import RocketCore from './RocketCore';
+import React, { useState, Component } from 'react'
+import RocketCore from './RocketCore'
 
 export function FunctionalRocket() {
-  const [initialLaunchTime] = useState(Date.now());
+  const [initialLaunchTime] = useState(Date.now())
 
-  return <RocketCore initialLaunchTime={initialLaunchTime} />;
+  return <RocketCore initialLaunchTime={initialLaunchTime} />
 }
 
 export class ClassRocket extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       initialLaunchTime: Date.now()
-    };
+    }
   }
 
   render() {
-    const { initialLaunchTime } = this.state;
+    const { initialLaunchTime } = this.state
 
-    return <RocketCore initialLaunchTime={initialLaunchTime} />;
+    return <RocketCore initialLaunchTime={initialLaunchTime} />
   }
 }
+
+//? Acá podría hacer lo mismo en el FunctionalRocket o en el ClassRocket, si modificamos los estados initialLaunchTime de ambos a un valor constante y no que varié con el tiempo como pasa con Date.now() ya no se harán re-render ni tampoco pasaran el estado actualizado al componente RocketCore.
